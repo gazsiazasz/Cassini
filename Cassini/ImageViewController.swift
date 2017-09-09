879cddb0a7cd2dd1fbe665aa6d633d8fb7cb4556
Gajdos Gábor <gazsiazasz@gmail.com>
2017-09-09 15:33:14 +0200

    Lecture 8

--- Cassini/ImageViewController.swift
+++ Cassini/ImageViewController.swift
@@ -9,42 +9,63 @@
 import UIKit
 
 class ImageViewController: UIViewController {
-
+    
+    // MARK: Model
+    
     var imageURL: URL? {
         didSet {
             image = nil
-            if view.window != nil {
-                fetchImage()
+            if view.window != nil { // if we're on screen
+                fetchImage()        // then fetch image
             }
         }
     }
     
+    // MARK: Private Implementation
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     private func fetchImage() {
         if let url = imageURL {
-            let urlContents = try? Data(contentsOf: url)
-            if let imageData = urlContents {
-                image = UIImage(data: imageData)
+            // this next line of code can throw an error
+            // and it also will block the UI entirely while access the network
+            // we really should be doing it in a separate thread
+            spinner.startAnimating()
+            DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+                let urlContents = try? Data(contentsOf: url)
+                
+                if let imageData = urlContents, url == self?.imageURL {
+                    DispatchQueue.main.async {
+                        self?.image = UIImage(data: imageData)
+                    }
+                }
             }
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageURL = DemoURL.stanford
+        // imageURL = DemoURL.stanford // for demo/testing purposes only
     }
+    
+    // MARK: View Controller Lifecycle
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        if image == nil {
-            fetchImage()
+        if image == nil { // we're about to appear on screen so, if needed,
+            fetchImage()  // fetch image
         }
     }
     
+    // MARK: User Interface
+    
     @IBOutlet weak var scrollView: UIScrollView! {
         didSet {
+            // to zoom we have to handle viewForZooming(in scrollView:)
             scrollView.delegate = self
+            // and we must set our minimum and maximum zoom scale
             scrollView.minimumZoomScale = 0.03
             scrollView.maximumZoomScale = 1.0
+            // most important thing to set in UIScrollView is contentSize
             scrollView.contentSize = imageView.frame.size
             scrollView.addSubview(imageView)
         }
@@ -59,11 +80,19 @@ class ImageViewController: UIViewController {
         set {
             imageView.image = newValue
             imageView.sizeToFit()
+            // careful here bevcause scrollView might be nil
+            // (for example , if we're setting our image as part of prepare)
+            // so use optional chaining to do nothing
             scrollView?.contentSize = imageView.frame.size
+            spinner?.stopAnimating()
         }
     }
 }
 
+// MARK: UIScrollViewDelegate
+// Extension which makes ImageViewController conform to UIScrollViewDelegate
+// Handles viewForZooming(in scrollView:)
+// by returning the UIImageView as the view to transform when zooming
 
 extension ImageViewController : UIScrollViewDelegate {
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
